e1f75e9db55d98a4188bb549a15e983aa9e34072
Katia Aresti <karesti@redhat.com>
2020-07-09 18:37:53 +0200

    start preparing docs

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+rm -rf /tmp/images
+
+#Generate diagrams
+asciidoctor -r asciidoctor-diagram -a generate-diagrams README.adoc
+
+#Generate HTML
+asciidoctor -a source-highlighter=pygments README.adoc
+
+mv images /tmp
+
+git checkout -f gh-pages
+mv /tmp/images/* images/
+mv README.html index.html
+git add -A
+git commit -m "Updated docs"
+git push origin gh-pages
+git checkout master
