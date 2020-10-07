c46304efdbf43542982cd3a601ac47a045405b99
Katia Aresti <karesti@redhat.com>
2020-10-07 15:38:38 +0200

    Upgrade Solution to 11.0.4.Final

---
+++ src/main/java/org/infinispan/tutorial/db/LocationWeatherQueryInitializer.java
@@ -0,0 +1,15 @@
+package org.infinispan.tutorial.db;
+
+import org.infinispan.protostream.SerializationContextInitializer;
+import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
+import org.infinispan.tutorial.data.LocationWeather;
+
+@AutoProtoSchemaBuilder(
+      includeClasses = {
+            LocationWeather.class
+      },
+      schemaFileName = "weather.proto",
+      schemaFilePath = "proto/",
+      schemaPackageName = "org.infinispan.tutorial.data")
+public interface LocationWeatherQueryInitializer extends SerializationContextInitializer {
+}
