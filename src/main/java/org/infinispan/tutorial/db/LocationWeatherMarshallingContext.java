814bacf28b4a9a0d8dc9ab085ec4d26e92f11d56
Katia Aresti <karesti@redhat.com>
2020-10-07 15:38:38 +0200

    Upgrade Solution to 11.0.4.Final

--- src/main/java/org/infinispan/tutorial/db/LocationWeatherMarshallingContext.java
+++ src/main/java/org/infinispan/tutorial/db/LocationWeatherMarshallingContext.java
@@ -2,15 +2,13 @@ package org.infinispan.tutorial.db;
 
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
-import org.infinispan.client.hotrod.marshall.MarshallerUtil;
-import org.infinispan.protostream.SerializationContext;
-import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
-import org.infinispan.tutorial.data.LocationWeather;
+import org.infinispan.client.hotrod.impl.query.RemoteQuery;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
 
-import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
-
-import static org.infinispan.query.remote.client.ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME;
 
 public final class LocationWeatherMarshallingContext {
 
@@ -18,28 +16,17 @@ public final class LocationWeatherMarshallingContext {
         System.out.println("---- LocationWeatherMarshallingContext - initialize the serialization context for LocationWeather class ----");
         Objects.requireNonNull(cacheManager);
 
-        // Get the serialization context of the client
-        SerializationContext ctx = MarshallerUtil.getSerializationContext(cacheManager);
-
-        // Use ProtoSchemaBuilder to define a Protobuf schema on the client
-        ProtoSchemaBuilder protoSchemaBuilder = new ProtoSchemaBuilder();
-        String fileName = "weather.proto";
-        String protoFile = null;
         try {
-            protoFile = protoSchemaBuilder
-                    .fileName(fileName)
-                    .addClass(LocationWeather.class)
-                    .packageName("org.infinispan.tutorial.data")
-                    .build(ctx);
-        } catch (IOException e) {
+            // Retrieve the generated Protobuf schema on the client
+            Path proto = Paths.get(RemoteQuery.class.getClassLoader()
+                  .getResource("proto/weather.proto").toURI());
+            // Retrieve metadata cache
+            RemoteCache<String, String> metadataCache =
+                  cacheManager.getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
+            // Define the new schema on the server too
+            metadataCache.put("weather.proto", Files.readString(proto));
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        // Retrieve metadata cache
-        RemoteCache<String, String> metadataCache =
-                cacheManager.getCache(PROTOBUF_METADATA_CACHE_NAME);
-
-        // Define the new schema on the server too
-        metadataCache.put(fileName, protoFile);
     }
 }
