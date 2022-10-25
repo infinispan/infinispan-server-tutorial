df7a48d8cc2ed908c1545fdbd2fec1275cbbd1cb
Katia Aresti <karesti@redhat.com>
2022-10-25 14:34:19 +0200

    Updates to 14.0.1.Final

--- src/main/java/org/infinispan/tutorial/data/LocationWeather.java
+++ src/main/java/org/infinispan/tutorial/data/LocationWeather.java
@@ -1,22 +1,30 @@
 package org.infinispan.tutorial.data;
 
+import org.infinispan.api.annotations.indexing.Basic;
+import org.infinispan.api.annotations.indexing.Indexed;
+import org.infinispan.api.annotations.indexing.Keyword;
 import org.infinispan.protostream.annotations.ProtoFactory;
 import org.infinispan.protostream.annotations.ProtoField;
 
 /**
  * Annotated with Protostream
  */
+@Indexed
 public class LocationWeather {
 
+   @Basic
    @ProtoField(number = 1, defaultValue = "0.0")
    float temperature;
 
+   @Basic
    @ProtoField(number = 2)
    String condition;
 
+   @Keyword(projectable = true, sortable = true, normalizer = "lowercase", indexNullAs = "unnamed", norms = false)
    @ProtoField(number = 3)
    String city;
 
+   @Keyword(projectable = true, sortable = true, normalizer = "lowercase", indexNullAs = "unnamed", norms = false)
    @ProtoField(number = 4)
    String country;
 
