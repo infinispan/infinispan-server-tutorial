27db478a3fb12b98ebe7808c4c5da705cd17bf4d
Katia Aresti <karesti@redhat.com>
2024-05-17 15:58:38 +0200

    Move main to 15

--- src/main/java/org/infinispan/tutorial/data/LocationWeather.java
+++ src/main/java/org/infinispan/tutorial/data/LocationWeather.java
@@ -3,56 +3,9 @@ package org.infinispan.tutorial.data;
 /**
  * Annotated with Protostream
  */
-public class LocationWeather {
-
-   //STEP Annotate the POJO LocationWeather
-
-   float temperature;
-
-   String condition;
-
-   String city;
-
-   String country;
-
-   public LocationWeather() {
-
-   }
-
-   public LocationWeather(float temperature, String condition, String city, String country) {
-      this.temperature = temperature;
-      this.condition = condition;
-      this.city = city;
-      this.country = country;
-   }
-
-   @Override
-   public String toString() {
-      return "LocationWeather{" +
-              "temperature=" + temperature +
-              ", condition='" + condition + '\'' +
-              ", city='" + city + '\'' +
-              ", country='" + country + '\'' +
-              '}';
-   }
-
-   public void setCondition(String condition) {
-      this.condition = condition;
-   }
-
-   public float getTemperature() {
-      return temperature;
-   }
-
-   public String getCondition() {
-      return condition;
-   }
-
-   public String getCity() {
-      return city;
-   }
-
-   public String getCountry() {
-      return country;
-   }
+//STEP Annotate the POJO LocationWeather
+public record LocationWeather(float temperature,
+                              String condition,
+                              String city,
+                              String country) {
 }
